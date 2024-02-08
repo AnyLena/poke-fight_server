1840c8639171f0baef1a9557255bc4dd37807276
LenaBultmann <lena.bultmann@gmx.de>
2024-02-08 15:25:16 +0100

    adding international Names

--- controllers/pokemon.js
+++ controllers/pokemon.js
@@ -3,7 +3,9 @@ import { pokemon } from "../data/data.js";
 
 export const getPokemons = async (req, res) => {
   const { offset, limit } = req.query;
-  console.log("Fetching pokemons", offset, limit)
+  // const offset = 0;
+  // const limit= 10;
+  // console.log("Fetching pokemons", offset, limit)
   try {
     const response = await axios.get(
       `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
@@ -12,23 +14,36 @@ export const getPokemons = async (req, res) => {
     const arrayOfPromises = results.map((poke) => {
       return axios.get(poke.url);
     });
+
+    const getAllPokeNames = async (url) => {
+      const response = await axios.get(url);
+      return response.data.names;
+    };
+
     const arrayOfPokemon = await Promise.all(arrayOfPromises);
-    const pokemons = arrayOfPokemon.map((poke) => {
-      return {
-        id: poke.data.id,
-        name: poke.data.name,
-        type: poke.data.types.map((type) => type.type.name),
-        base: {
-          hp: poke.data.stats[0].base_stat,
-          attack: poke.data.stats[1].base_stat,
-          defense: poke.data.stats[2].base_stat,
-          special_attack: poke.data.stats[3].base_stat,
-          special_defense: poke.data.stats[4].base_stat,
-          speed: poke.data.stats[5].base_stat,
-        },
-        sprites: poke.data.sprites,
-      };
-    });
+    const pokemons = await Promise.all(
+      arrayOfPokemon.map(async (poke) => {
+        const allNames = await getAllPokeNames(poke.data.species.url);
+
+        return {
+          id: poke.data.id,
+          name: {
+            en: poke.data.name,
+            other: allNames,
+          },
+          type: poke.data.types.map((type) => type.type.name),
+          base: {
+            hp: poke.data.stats[0].base_stat,
+            attack: poke.data.stats[1].base_stat,
+            defense: poke.data.stats[2].base_stat,
+            special_attack: poke.data.stats[3].base_stat,
+            special_defense: poke.data.stats[4].base_stat,
+            speed: poke.data.stats[5].base_stat,
+          },
+          sprites: poke.data.sprites,
+        };
+      })
+    );
     res.json(pokemons);
   } catch (error) {
     console.log(error);
